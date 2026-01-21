afa2e044b6e566746c9fea4ef3895009f816a1ed
Anthony <anthony.pradier04@gmail.com>
2026-01-22 00:19:38 +0100

    start parsing scene

--- core/scene.py
+++ core/scene.py
@@ -1,6 +1,8 @@
 import json
+from typing import List, Tuple
 from scenes._sceneKeys import *
 from objects.sphere_utils import Sphere
+from objects.Cube import Cube
 from lights.PointLight import PointLight
 from lights.DirLight import DirLight
 from lights.AmbientLight import AmbientLight
@@ -11,29 +13,20 @@ from animations.LinearMove import LinearMove
 
 class Scene:
     def __init__(self):
-        red_sphere = Sphere(center=(-1, -1, 3), radius=1, color=(255, 0, 0), specular=500, reflective=0.2)
-        blue_sphere = Sphere(center=(1, 0, 4), radius=1, color=(0, 0, 255), specular=500, reflective=0.3)
+        # red_sphere = Sphere(center=(-1, -1, 3), radius=1, color=(255, 0, 0), specular=500, reflective=0.2)
+        # blue_sphere = Sphere(center=(1, 0, 4), radius=1, color=(0, 0, 255), specular=500, reflective=0.3)
         
-        self.objects = [
-            red_sphere,
-            blue_sphere,
-            Sphere(center=(-2, 0, 4), radius=1, color=(0, 255, 0), specular=10, reflective=0.4),
-            Plane(point=(0.0, -1.5, 0.0), normal=(0.0, 1.0, 0.0), color=(255, 255, 0), reflective=0.1)
-        ]
+        self.objects = []
 
-        self.lights = [
-            PointLight(position=(2.0, 1.0, 2.0), intensity=(0.7, 0.7, 0.7)),
-            DirLight(direction=(-1.0, -1.0, 1.0), intensity=(0.2, 0.2, 0.2)),
-            AmbientLight(intensity=(0.2, 0.2, 0.2))
-        ]
+        self.lights = []
         
         self.camera = Camera(canvas_width=1200, canvas_height=800)
-        self.animation_steps = 60
+        self.animation_steps = 1
         
-        self.animations = [
-            SphereScaling(self, red_sphere, 0.5, 60),
-            LinearMove(self, blue_sphere, (1, 1, 0), 60)
-        ]
+        # self.animations = [
+        #     SphereScaling(self, red_sphere, 0.5, 60),
+        #     LinearMove(self, blue_sphere, (1, 1, 0), 60)
+        # ]
         
         
     def update_frame(self, step):
@@ -55,12 +48,36 @@ class Scene:
             name = filename.split("scenes/")[1]
         
         try:
+            print("Loading " + name + "...")
             with open("./scenes/" + name, 'r') as file:
                 scene = json.load(file)
-            spheres: list = scene[OBJECTS][SPHERES]
-            planes: list = scene[OBJECTS][PLANES]
-            lights: list = scene[LIGHTS]
-            
-            
+            self.parse_scene(scene)
+                
         except:
             raise FileNotFoundError("The specified file/path doesn't exists")
+    
+    def parse_scene(self, scene):
+            spheres: List[dict] = scene[OBJECTS][SPHERES]
+            planes: List[dict] = scene[OBJECTS][PLANES]
+            cubes: List[dict] = scene[OBJECTS][CUBES]
+            
+            point_lights: List[dict] = scene[LIGHTS][POINTS]
+            directional_lights: List[dict] = scene[LIGHTS][DIRECTIONALS]
+            ambient_lights: List[dict] = scene[LIGHTS][AMBIENTS]
+            
+            if len(spheres != 0):
+                for sphere in spheres:
+                    s: Sphere = self.parseSphere(sphere)
+            if len(planes != 0):
+                for plane in planes:
+                    p: Plane = self.parsePlane(plane)
+            if len(cubes != 0):
+                for cube in cubes:
+                    c: Cube = self.parseCube(plane)
+            
+            if len(point_lights != 0):
+                pass
+            if len(directional_lights != 0):
+                pass
+            if len(ambient_lights != 0):
+                pass
